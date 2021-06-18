88ae178268e2459645a421a85b08ddd5061f7e7e
Daniela <danaa.p@live.com.ar>
2021-06-18 16:06:33 -0300

    Avances

---
+++ web/javaScript/aprendiendo-jquery/js/03-textos.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+   reloadLinks();
+
+    $('#add_button').removeAttr('disabled')
+                    .click(function(){
+       //Para agregarlo al final
+     //$('#menu').append('<li><a href="'+$('#add_link').val()+'"></a></li>');
+     //para agregarlo al principio
+     $('#menu').prepend('<li><a href="'+$('#add_link').val()+'"></a></li>');
+     $('#add_link').val('')
+     reloadLinks();
+   });
+
+
+
+
+   
+  
+});
+
+function reloadLinks(){
+      console.log($('a').length);
+      $('a').each(function(index){
+         var dom = $(this);
+         var enlace = dom.attr('href');
+         dom.attr('target','_blank');
+         dom.text(enlace);
+      })
+
+   }
